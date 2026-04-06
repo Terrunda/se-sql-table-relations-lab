2ae6480b4cd941b35be3212c5802c7258b1d0b5d
Terrunda <terrencegift49@gmail.com>
2026-04-06 22:10:38 +0300

    Revert "Completed."
    This reverts commit 67360e3e2e8b6ca94ffee61956e0f658ffaedf8c.

--- main.py
+++ main.py
@@ -11,101 +11,42 @@ pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 
 # STEP 1
 # Replace None with your code
-df_boston = "SELECT firstName, lastName, jobTitle FROM employees WHERE officeCode = '2'"
+df_boston = None
 
+# STEP 2
 # Replace None with your code
-df_zero_emp = "SELECT * FROM employees WHERE officeCode NOT IN (SELECT DISTINCT officeCode FROM employees)"
+df_zero_emp = None
 
 # STEP 3
 # Replace None with your code
-df_employee = "SELECT e.firstName, e.lastName, o.city, o.state FROM employees e LEFT JOIN offices o ON e.officeCode = o.officeCode ORDER BY e.firstName, e.lastName"
+df_employee = None
 
 # STEP 4
 # Replace None with your code
-df_contacts = "SELECT contactFirstName, contactLastName, phone, salesRepEmployeeNumber FROM customers c LEFT JOIN orders o ON c.customerNumber = o.customerNumber WHERE o.orderNumber IS NULL ORDER BY contactLastName ASC;"
+df_contacts = None
 
 # STEP 5
 # Replace None with your code
-df_payment = """
-SELECT 
-    c.contactFirstName, 
-    c.contactLastName, 
-    p.paymentDate, 
-    p.amount
-FROM customers c
-JOIN payments p ON c.customerNumber = p.customerNumber;
-"""
+df_payment = None
 
 # STEP 6
 # Replace None with your code
-df_credit = """
-SELECT 
-    e.employeeNumber, 
-    e.firstName, 
-    e.lastName, 
-    COUNT(c.customerNumber) AS numberOfCustomers
-FROM employees e
-JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
-GROUP BY e.employeeNumber, e.firstName, e.lastName
-HAVING AVG(c.creditLimit) > 90000
-ORDER BY numberOfCustomers DESC;
-"""
+df_credit = None
 
 # STEP 7
 # Replace None with your code
-df_product_sold = """
-SELECT 
-    p.productName, 
-    COUNT(od.orderNumber) AS numorders, 
-    SUM(od.quantityOrdered) AS totalunits
-FROM products p
-JOIN orderdetails od ON p.productCode = od.productCode
-GROUP BY p.productName
-ORDER BY totalunits DESC;
-"""
+df_product_sold = None
 
 # STEP 8
 # Replace None with your code
-df_total_customers ="""
-SELECT 
-    p.productName, 
-    p.productCode, 
-    COUNT(DISTINCT o.customerNumber) AS numpurchasers
-FROM products p
-JOIN orderdetails od ON p.productCode = od.productCode
-JOIN orders o ON od.orderNumber = o.orderNumber
-GROUP BY p.productName, p.productCode
-ORDER BY numpurchasers DESC;
-"""
+df_total_customers = None
 
 # STEP 9
 # Replace None with your code
-df_customers = """
-SELECT 
-    o.officeCode, 
-    o.city, 
-    COUNT(c.customerNumber) AS n_customers
-FROM offices o
-LEFT JOIN employees e ON o.officeCode = e.officeCode
-LEFT JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
-GROUP BY o.officeCode, o.city
-ORDER BY n_customers DESC;"""
+df_customers = None
 
 # STEP 10
 # Replace None with your code
-df_under_20 = """
-SELECT DISTINCT 
-    e.employeeNumber, 
-    e.firstName, 
-    e.lastName, 
-    off.city, 
-    off.officeCode,
-    od.productCode
-FROM employees e
-JOIN offices off ON e.officeCode = off.officeCode
-JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
-JOIN orders o ON c.customerNumber = o.customerNumber
-JOIN orderdetails od ON o.orderNumber = od.orderNumber
-"""
+df_under_20 = None
 
 conn.close()
